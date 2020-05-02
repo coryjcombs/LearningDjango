9507dac4cf23e466d8a0113354c03f206adc066e
coryjcombs <cory.j.combs@outlook.com>
2020-05-02 11:29:30 -0400

    Installed admindocs and added back end functionality

--- DjangoWeb/DjangoWeb/urls.py
+++ DjangoWeb/DjangoWeb/urls.py
@@ -4,9 +4,11 @@ Definition of urls for DjangoWeb.
 
 from datetime import datetime
 from django.urls import path
+from django.conf.urls import include
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+admin.autodiscover()
 
 
 urlpatterns = [
@@ -27,4 +29,5 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
 ]
