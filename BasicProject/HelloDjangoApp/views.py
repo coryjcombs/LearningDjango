a1b62455ea347cfcd450d6bdd8aba276011fde91
coryjcombs <cory.j.combs@outlook.com>
2020-05-01 21:59:15 -0400

    Created dynamic home view

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -5,8 +5,12 @@ from datetime import datetime
 def index(request):
     now = datetime.now()
 
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello, Django!</strong> This page was last loaded on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
-    
-    return HttpResponse(html_content)
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {
+            'title': "Hello Django",
+            'message': "Hello Django!",
+            'content': "This page was last loaded on " + now.strftime("%A, %d %B, %Y at %X") + "."
+        }
+    )
