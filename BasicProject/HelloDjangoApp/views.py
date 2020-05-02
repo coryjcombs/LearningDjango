26c4befff091a46c19242363f64a28bb58a7ddb5
coryjcombs <cory.j.combs@outlook.com>
2020-05-01 21:38:05 -0400

    Created timed view

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>Hello, Django!</strong> This page was last loaded on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+    
+    return HttpResponse(html_content)
